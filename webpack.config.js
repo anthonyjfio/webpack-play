22fa41098bd7cc32e8297eefe4a268ce85773e29
Anthony <ajfiorani@gmail.com>
2015-10-29 21:33:26 -0400

    add extract text plugin, add open browser plugin, add html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var HtmlWebpack = require('html-webpack-plugin')
+    OpenBrowser = require('open-browser-webpack-plugin')
+    ExtractText = require('extract-text-webpack-plugin')
 
 module.exports = {
 
@@ -13,16 +15,38 @@ module.exports = {
     loaders: [
 
       {
-        test: /(\.js$|\.jsx$)/,
+        test: /\.(js|jsx|babel)$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
       },
 
       {
         test: /\.css$/,
-        loaders: ['style', 'css', 'cssnext', 'autoprefixer']
-      }
+        loader: ExtractText.extract(
+          'style',
+          'css!cssnext!autoprefixer'
+        )
+      },
 
+      // {
+      //   test: /\.css$/,
+      //   loaders: [
+      //     'style',
+      //     'css',
+      //     'cssnext',
+      //     'autoprefixer'
+      //   ]
+      // },
+
+      {
+        test: /\.(otf|eot|ttf|woff)$/,
+        loader: 'url-loader?limit=8192'
+      },
+
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        loader: 'url-loader?limit=8192'
+      }
     ]
   },
 
@@ -30,14 +54,22 @@ module.exports = {
     contentBase: 'src/',
     historyApiFallback: true,
     hot: true,
-    // inline: true,
-    progress: true
+    progress: true,
+    debug: true
   },
 
   plugins: [
     new HtmlWebpack({
-      title: 'Anthony webpack'
-    })
+      title: 'Anthony webpack',
+      author: 'Anthony Fiorani',
+      description: 'testing out webpack',
+      template: 'src/index.html',
+      inject: true
+    }),
+    new OpenBrowser({
+      url: 'http://localhost:8080/webpack-dev-server/'
+    }),
+    new ExtractText('[name].css')
   ]
 
 }
